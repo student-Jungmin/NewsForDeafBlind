fca8a3790d28c6127310e32c73cd725551c556ce
ohjinho <ohjinho7@gmail.com>
2021-10-26 12:27:44 +0900

    프로젝트 시작

---
+++ NewForBlind/src/main/java/com/ssuOpensource/NewForBlind/NewForBlindApplication.java
@@ -0,0 +1,13 @@
+package com.ssuOpensource.NewForBlind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewForBlindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewForBlindApplication.class, args);
+	}
+
+}
---
+++ NewForBlind/src/test/java/com/ssuOpensource/NewForBlind/NewForBlindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssuOpensource.NewForBlind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewForBlindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
