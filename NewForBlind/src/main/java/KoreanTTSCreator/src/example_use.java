d1264ed8023107a14dd88e6142d9698d89f335ce
박동진 <81606583+furu23@users.noreply.github.com>
2021-11-10 17:03:31 +0900

    Update example_use.java

--- NewForBlind/src/main/java/KoreanTTSCreator/src/example_use.java
+++ NewForBlind/src/main/java/KoreanTTSCreator/src/example_use.java
@@ -2,7 +2,7 @@ public class example_use {
     public static void main(String[] args) {
         TextToSpeach test = new TextToSpeach();
         try {
-            test.textToSpeach("8d011ccde2f54638b2c629e02882955c");
+            test.textToSpeach("여기에 키 넣기");
         } catch (Exception e) {
             e.printStackTrace();
         }
