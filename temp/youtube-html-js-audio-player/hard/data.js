185a4f847a95b929b62b7bb2315b351cb4220527
안태욱 <55526071+AnTaeWook@users.noreply.github.com>
2021-11-19 22:37:55 +0900

    Add files via upload

---
+++ temp/youtube-html-js-audio-player/hard/data.js
@@ -0,0 +1,8 @@
+export default [
+  {
+    title: "Júnior Pleno ou Sênior, qual a diferença?",
+    artist: "Diego Fernandes",
+    cover: "junior-pleno-ou-senior-qual-a-diferenca.jpg",
+    file: "policy.mp3"
+  }
+];
