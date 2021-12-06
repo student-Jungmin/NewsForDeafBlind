f0098fb1dbc9f0787ccafcec71fe6e989966460f
박동진 <81606583+furu23@users.noreply.github.com>
2021-12-06 16:28:06 +0900

    jumja fix

--- NewForBlind/src/main/java/com/ssuOpensource/NewForBlind/common/h2b_ex.java
+++ NewForBlind/src/main/java/com/ssuOpensource/NewForBlind/common/h2b_ex.java
@@ -3,7 +3,7 @@ package com.ssuOpensource.NewForBlind.common;
 public class h2b_ex {
     public static void main(String[] args) {
         HtoB h2b = new HtoB();
-        String str = h2b.H2B("안녕하세요");
+        String str = h2b.H2B("맑은 eAB12c 정신 11Bae?!@@#!");
         System.out.println(str);
     }
 }
